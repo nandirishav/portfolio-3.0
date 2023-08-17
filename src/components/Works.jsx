d034cad09ddd4dd245b499a8cdecd06a71c53e56
RISHAV\USER <rishavnandi06@gmail.com>
2023-08-17 22:33:51 +0530

    WIP

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -72,16 +72,14 @@ const ProjectCard = ({
 const Works = () => {
   return (
     <>
-      <div
-      //  variants={textVariant()}
-      >
+      <motion.div variants={textVariant()}>
         <p className={`${styles.sectionSubText} `}>My work</p>
         <h2 className={`${styles.sectionDarkHeadText}`}>Projects.</h2>
-      </div>
+      </motion.div>
 
       <div className="w-full flex">
-        <p
-          // variants={fadeIn("", "", 0.1, 1)}
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
           Following projects showcases my skills and experience through
@@ -89,7 +87,7 @@ const Works = () => {
           links to code repositories and live demos in it. It reflects my
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
-        </p>
+        </motion.p>
       </div>
 
       <div className="mt-20 flex flex-wrap gap-7">
