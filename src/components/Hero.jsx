e85ea064981988ffc1afe0f004bd6941efad915d
RISHAV\USER <rishavnandi06@gmail.com>
2023-08-17 16:15:09 +0530

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -86,7 +86,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      {/* <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
@@ -102,7 +102,7 @@ const Hero = () => {
             />
           </div>
         </a>
-      </div>
+      </div> */}
     </section>
   );
 };
